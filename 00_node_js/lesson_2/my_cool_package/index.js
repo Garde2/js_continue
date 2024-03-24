231b8b6ff03a3a7487b659029e87f62c5c2aa0ee
Garde2 <33912422+Garde2@users.noreply.github.com>
2024-03-24 22:21:42 +0300

    fixed my_cool_package

--- 00_node_js/lesson_2/my_cool_package/index.js
+++ 00_node_js/lesson_2/my_cool_package/index.js
@@ -9,4 +9,20 @@ function multiply(x, y) {
   return x * y;
 }
 
-module.exports = { add, subtract, multiply };
+function quadraticWithTwoUnknown(a, b, c) {
+  let discriminant = b * b - 4 * a * c;
+
+  if (discriminant < 0) {
+    return "Уравнение не имеет реальных корней";
+  } else if (discriminant === 0) {
+    return "Уравнение имеет один корень: " + -b / (2 * a);
+  } else {
+    let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+
+    let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+    return "Уравнение имеет два корня: " + root1 + " и " + root2;
+  }
+}
+
+module.exports = { add, subtract, multiply, quadraticWithTwoUnknown };
