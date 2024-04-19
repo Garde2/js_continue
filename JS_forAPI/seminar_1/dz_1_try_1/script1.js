7629c6d88c3ba2f2a96559b9fa5992df5e26201e
Garde2 <33912422+Garde2@users.noreply.github.com>
2024-04-19 19:21:44 +0300

    прогресс есть, но запись юзеров еще не алё

--- JS_forAPI/seminar_1/dz_1_try_1/script1.js
+++ JS_forAPI/seminar_1/dz_1_try_1/script1.js
@@ -26,48 +26,60 @@
 
 const initialLessons = `[
   {
-    id: 1,
-    name: "Йога",
-    time: "10:00 - 11:00",
-    maxParticipants: 15,
-    currentParticipants: 8,
+      "id": 1,
+      "name": "Йога",
+      "time": "10:00 - 11:00",
+      "maxParticipants": 15,
+      "currentParticipants": 8
   },
   {
-    id: 2,
-    name: "Пилатес",
-    time: "11:30 - 12:30",
-    maxParticipants: 10,
-    currentParticipants: 5,
+      "id": 2,
+      "name": "Пилатес",
+      "time": "11:30 - 12:30",
+      "maxParticipants": 10,
+      "currentParticipants": 5
   },
   {
-    id: 3,
-    name: "Кроссфит",
-    time: "13:00 - 14:00",
-    maxParticipants: 20,
-    currentParticipants: 15,
+      "id": 3,
+      "name": "Кроссфит",
+      "time": "13:00 - 14:00",
+      "maxParticipants": 20,
+      "currentParticipants": 15
   },
   {
-    id: 4,
-    name: "Танцы",
-    time: "14:30 - 15:30",
-    maxParticipants: 12,
-    currentParticipants: 10,
+      "id": 4,
+      "name": "Танцы",
+      "time": "14:30 - 15:30",
+      "maxParticipants": 12,
+      "currentParticipants": 10
   },
   {
-    id: 5,
-    name: "Бокс",
-    time: "16:00 - 17:00",
-    maxParticipants: 8,
-    currentParticipants: 6,
-  },
+      "id": 5,
+      "name": "Бокс",
+      "time": "16:00 - 17:00",
+      "maxParticipants": 8,
+      "currentParticipants": 6
+  }
 ]`;
 
+const coursesData = {};
+const coursesList = JSON.parse(initialLessons);
+
 const lessonsKey = "lessons";
-const lsUsersKey = "users";
-const lessonEl = document.querySelector(".lessons-table");
+const usersKey = "users";
+const lessonElement = document.querySelector(".lessons-table");
+
+coursesList.forEach((course) => {
+  const { id, ...lesson } = course;
+  coursesData[id] = { ...lesson };
+});
 
 if (!localStorage.getItem(lessonsKey)) {
-  localStorage.setItem(lessonsKey, JSON.stringify(initialLessons));
+  saveLessons(lessonsKey, coursesData);
+}
+
+function saveLessons(lessonsKey, object) {
+  localStorage.setItem(lessonsKey, JSON.stringify(object));
 }
 
 const lessons = getLessons();
@@ -76,16 +88,23 @@ function getLessons() {
   return JSON.parse(localStorage.getItem(lessonsKey));
 }
 
-lessons.forEach((lesson) => {
-  lessonEl.insertAdjacentHTML("beforeend", getLessonHtml(lesson));
-});
+for (const id in lessons) {
+  // const { id, name, time, maxParticipants, currentParticipants } = lessons[id];
+  // lesson.insertAdjacentHTML(
+  //   "beforeend",
+  //   getLessonHtml({ id, name, time, maxParticipants, currentParticipants })
+  // );
+  updatedlesson(lessons[id], id);
+}
+
+function updatedlesson(lesson, id) {
+  lessonElement.insertAdjacentHTML("beforeend", getLessonHtml(lesson, id));
+}
 
 //тут у нас таблица
-function getLessonHtml(lesson) {
-  return `
-  <table>
-    <p class="article" data-id="${lesson.id}"></p>
-    <td>
+function getLessonHtml(lesson, id) {
+  return `  
+    <div class="lesson" data-id="${id}">    
         <h3 class="title">${lesson.name}</h3>
         <p class="time">${lesson.time}</p>
         <p class="maxPart-name">Максимальное кол-во участников:</p>
@@ -93,9 +112,8 @@ function getLessonHtml(lesson) {
         <p class="maxPart-name">Число записавшихся:</p>
         <p class="curPart">${lesson.currentParticipants}</p>
         <button class="removeBtn">Отменить запись</button>
-        <button class="joinBtn">Записаться на урок</button>
-    </td>
-  </table>
+        <button class="joinBtn">Записаться на урок</button>  
+  </div>
     `;
 }
 
@@ -103,30 +121,28 @@ function getLessonHtml(lesson) {
 //будем писать как на семинаре промпт вводить никнейм
 
 const userName = prompt("Введите ваше имя/никнейм");
-if (!localStorage.getItem(lsUsersKey)) {
-  localStorage.setItem(lsUsersKey, JSON.stringify([]));
+if (!localStorage.getItem(usersKey)) {
+  saveLessons(usersKey, {});
 }
 //если участник уже записан, то ничего не делаем,если имя новое - добавляем в список
 //но ничо не получается
 
-let users = JSON.parse(localStorage.getItem(lsUsersKey));
-if (!users.hasOwnProperty(userName)) {
+let users = JSON.parse(localStorage.getItem(usersKey));
+if (!users[userName]) {
   users[userName] = {};
-
-  users.push(lsUsersKey, userName);
-  saveLessons(lsUsersKey, users);
+  saveLessons(usersKey, users);
 }
 
 window.addEventListener("DOMContentLoaded", (e) => {
   const userLesson = users[userName];
   for (const lessonId in lessons) {
-    const lessonEl = document.querySelector(`.lesson[data-id="${lessonId}"]`);
+    const lesson = document.querySelector(`.lesson[data-id="${lessonId}"]`);
     if (userLesson.hasOwnProperty(lessonId)) {
-      const joinBtn = lessonEl.querySelector(".joinBtn");
+      const joinBtn = lesson.querySelector(".joinBtn");
       joinBtn.disabled = true;
     } else {
-      const removeBtn = lessonEl.querySelector(".removeBtn"); //а ты почему ты null?!
-      removeBtn.disabled = true;
+      const deleteBtn = lesson.querySelector(".removeBtn");
+      deleteBtn.disabled = true;
     }
   }
 });
@@ -135,31 +151,30 @@ window.addEventListener("DOMContentLoaded", (e) => {
 //и проверяется, что количество currentParticipants не превышает maxParticipants. Если currentParticipants равно maxParticipants или пользователь уже записан на занятие, то кнопка Записаться становится неактивной.
 
 //не там у меня никнейм проверяется, надо чтоб при попытк зарегиться
-lessonEl.addEventListener("click", ({ target }) => {
-  const lessonEl = target.closest(".lessons");
-  const lessonId = lessonEl.dataset.id; //почему ты null?!
-
-  if (target.matches(".removeBtn")) {
-    //если участник записан и хочет отменить запись, то после нажатия кнопки Отменить запись один вычитается из currentParticipants, кнопка Записаться становится активной, а кнопка Отменить запись - неактивной.
-    if (!checkUsers(lessonId)) {
-      target.disabled = true;
-      return;
-    }
-    removeBtn(lessonEl);
-    const joinBtn = lessonEl.querySelector(".joinBtn");
-    joinBtn.disabled = false;
-    target.disabled = true;
-    return;
-  }
+lessonElement.addEventListener("click", ({ target }) => {
+  const lesson = target.closest(".lesson");
+  const lessonId = lesson.dataset.id;
+  //если участник записан и хочет отменить запись, то после нажатия кнопки Отменить запись один вычитается из currentParticipants, кнопка Записаться становится активной, а кнопка Отменить запись - неактивной.
   if (target.matches(".joinBtn")) {
     if (checkParicipants(lessonId) || checkUsers(lessonId)) {
       target.disabled = true;
       return;
     }
-    joinBtn(lessonEl);
+    joinBtn(lesson);
     target.disabled = true;
-    const removeBtn = lessonEl.querySelector(".removeBtn");
-    removeBtn.disabled = false;
+    const deleteBtn = lesson.querySelector(".removeBtn");
+    deleteBtn.disabled = false;
+    return;
+  }
+  if (target.matches(".removeBtn")) {
+    if (!checkUsers(lessonId)) {
+      target.disabled = true;
+      return;
+    }
+    removeSign(lesson);
+    const joinBtn = lesson.querySelector(".joinBtn");
+    joinBtn.disabled = false;
+    target.disabled = true;
     return;
   }
 
@@ -176,42 +191,37 @@ lessonEl.addEventListener("click", ({ target }) => {
   // return;
 });
 
-function joinBtn(lessonEl) {
-  const lessonId = lessonEl.dataset.id;
-  lessons[userId].currentParticipants += 1;
+function joinBtn(lesson) {
+  const lessonId = lesson.dataset.id;
+  lessons[lessonId].currentParticipants++;
   const { name, time } = lessons[lessonId];
   users[userName][lessonId] = { name, time };
-
-  generalSave();
-}
-
-function removeBtn(lessonEl) {
-  const userId = lessonEl.dataset.id;
-  lessons[userId].currentParticipants -= 1;
-  delete users[userName][userId];
-
-  generalSave();
-}
-
-function generalSave() {
   saveLessons(lessonsKey, lessons);
-  saveLessons(lsUsersKey, users);
-  const currentParticipants = lessonEl.querySelector(".curPart");
+  saveLessons(usersKey, users);
+  generalUpdate(lesson);
+}
+
+function removeSign(lesson) {
+  const id = lesson.dataset.id;
+  lessons[id].currentParticipants--;
+  delete users[userName][id];
+  saveLessons(lessonsKey, lessons);
+  saveLessons(usersKey, users);
+  generalUpdate(lesson);
+}
+
+function generalUpdate(lesson) {
+  const currentParticipants = lesson.querySelector(".curPart");
   currentParticipants.textContent =
-    lessons[lessonEl.dataset.id].currentParticipants;
+    lessons[lesson.dataset.id].currentParticipants;
 }
 
 function checkParicipants(lessonId) {
-  return lessonId.currentParticipants < lessonId.maxParticipants;
+  return (
+    lessons[lessonId].maxParticipants > lessons[lessonId].currentParticipants
+  );
 }
 
 function checkUsers(lessonId) {
-  //???
-  return users[userName].hasOwnProperty(lessonId);
+  return users[userName][lessonId];
 }
-
-function saveLessons(lessonsKey, lessons) {
-  localStorage.setItem(lessonsKey, JSON.stringify(lessons));
-}
-
-//2:02:01  про свежие данные если 2 страницы
